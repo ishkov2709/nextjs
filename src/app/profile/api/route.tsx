511c8bca8904949b91526ca7dc39f5b3b8e2d932
ishkov2709 <dmitryishkov27@gmail.com>
2024-01-19 13:14:46 +0200

    add cookie handler

--- src/app/profile/api/route.tsx
+++ src/app/profile/api/route.tsx
@@ -1,10 +1,14 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const headersList = headers();
 
-  console.log(headersList.get("Authorization"));
+  const token = headersList.get("Authorization");
+
+  cookies().set("JWT", String(token));
+
+  console.log(cookies().get("JWT"));
 
   return new Response("<h1>Profile API data</h1>", {
     headers: {
